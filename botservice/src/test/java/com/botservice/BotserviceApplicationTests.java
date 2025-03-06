59b0ee9615074e6527b1e0e23af683bc730d3975
strangecodder <strangecodder@gmail.com>
2025-03-06 15:18:56 +0300

    init project

---
+++ botservice/src/test/java/com/botservice/BotserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
