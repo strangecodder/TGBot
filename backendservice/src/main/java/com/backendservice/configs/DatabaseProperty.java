b1aeb88ed967cc37e3aa2965c2a66907c8c8f329
strangecodder <strangecodder@gmail.com>
2025-03-17 20:34:08 +0300

    feat: класс для пропертей бд

---
+++ backendservice/src/main/java/com/backendservice/configs/DatabaseProperty.java
@@ -0,0 +1,15 @@
+package com.backendservice.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DatabaseProperty {
+
+    private String username;
+    private String password;
+    private String url;
+    private String classDriver;
+
+}
