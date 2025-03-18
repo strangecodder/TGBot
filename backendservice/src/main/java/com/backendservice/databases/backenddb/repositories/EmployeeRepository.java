443db95ad96ec2b6686a17b707c7440b8a1242a9
strangecodder <strangecodder@gmail.com>
2025-03-18 22:53:38 +0300

    коммит без комментариев

---
+++ backendservice/src/main/java/com/backendservice/databases/backenddb/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.backendservice.databases.backenddb.repositories;
+
+import com.backendservice.databases.backenddb.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
