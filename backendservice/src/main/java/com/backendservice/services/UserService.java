58f21fb569cb7d54df431b192007f62d8a198ba7
strangecodder <strangecodder@gmail.com>
2025-03-19 08:14:42 +0300

    feature: добавил метод для проверки

--- backendservice/src/main/java/com/backendservice/services/UserService.java
+++ backendservice/src/main/java/com/backendservice/services/UserService.java
@@ -1,8 +1,12 @@
 package com.backendservice.services;
 
+import com.backendservice.databases.backenddb.entities.Employee;
 import com.backendservice.databases.backenddb.entities.State;
+import com.backendservice.databases.backenddb.repositories.EmployeeRepository;
 import com.backendservice.databases.backenddb.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +18,11 @@ public class UserService {
     @Autowired
     private StateRepository stateRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     //@Transactional(transactionManager = "backendTransactionManager")
+    // ТЕСТОВЫЙ МЕТОД ДЛЯ ПРОВЕРКИ БД
     public void saveAnother(){
         Optional<State> optional = stateRepository.findById(1);
 
@@ -22,5 +30,16 @@ public class UserService {
         state.setId(1);
         state.setValue("lll");
         stateRepository.save(state);
+    }
+
+/*    public ResponseEntity<HttpStatus> createEmployee(EmployeeDTO employeeDTO){
+
+    }*/
+
+    public ResponseEntity<Employee> getEmployeeByTgId(long tgId){
+        if(!employeeRepository.existsByTg_id()){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(employeeRepository.findEmployeeByTg_id(),HttpStatus.OK);
     }
 }
