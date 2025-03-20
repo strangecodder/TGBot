2a3117ef494cf7b3c7b7efd021d836740c60f503
strangecodder <strangecodder@gmail.com>
2025-03-21 00:40:53 +0300

    feat: добавил логику отправки сообщения

--- botservice/src/main/java/com/botservice/telegram/commands/StartCommand.java
+++ botservice/src/main/java/com/botservice/telegram/commands/StartCommand.java
@@ -1,8 +1,11 @@
 package com.botservice.telegram.commands;
 
+import com.botservice.dto.EmployeeDTO;
+import com.botservice.services.RegistrationService;
 import com.botservice.utils.CommandConsts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -12,6 +15,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @RequiredArgsConstructor
 public class StartCommand implements Command{
 
+    @Autowired
+    private RegistrationService registrationService;
+
     @Override
     public SendMessage apply(Update update){
         long chatId = update.getMessage().getChatId();
@@ -20,6 +26,11 @@ public class StartCommand implements Command{
         sendMessage.setText(CommandConsts.START_MESSAGE);
 
         addKeyboard(sendMessage);
+
+
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        employeeDTO.setId(chatId);
+        registrationService.sendEmployeeData("Ввёл команду /start");
         return sendMessage;
     }
 
